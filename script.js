2141d1cbe36ed00a8776a689292cc3ce13c32bd8
imstellar <182440248+imstellar@users.noreply.github.com>
2025-04-30 14:40:54 +0100

    Update script.js
    - Added logic fix

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
-    navBarButtonHighlight();});
+window.addEventListener('load', function () {
+    navBarButtonHighlight()
+})
 
 function navBarButtonHighlight() {
     const totalButtons = document.querySelectorAll("#nav-button");
     const currentPage = window.location.pathname.split('/').pop();
+
     let i = 0
 
     while (i < 4) {
         const button = totalButtons[i];
         const page = button.getAttribute("data-page");
-        
+
         if (page == currentPage) {
             console.log("Page button found")
             button.classList.add("btn-focused")
             button.classList.remove("btn-dark")
+            i = 4
+        } else {
+            console.log("Page button mismatch")
+            i++
         }
-        i++
     }
 }
